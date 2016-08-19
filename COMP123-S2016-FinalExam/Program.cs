f2b6e9c3dbff2b3e3766c2094655ba7660e0f51f
300872772 <mamun_131@hotmail.com>
2016-08-19 10:59:05 -0300

    added all properties to splash form

--- COMP123-S2016-FinalExam/Program.cs
+++ COMP123-S2016-FinalExam/Program.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ *App Name: Character Builder 
+ *Author: Md Mamunur Rahman 
+ * Student ID: 300872772  
+ * 
+ * Date last modified: August 09, 2016 
+ * Description: This application demonstrates a Solution for character building 
+ *  
+ * Version: 0.0.1 - added all properties to splash form
+ */
 namespace COMP123_S2016_FinalExam
 {
     public static class Program
@@ -19,7 +28,7 @@ namespace COMP123_S2016_FinalExam
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AbilityGeneratorForm());
+            Application.Run(new SplashForm());
         }
     }
 }
---
+++ COMP123-S2016-FinalExam/SplashForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ *App Name: Character Builder 
+ *Author: Md Mamunur Rahman 
+ * Student ID: 300872772  
+ * 
+ * Date last modified: August 09, 2016 
+ * Description: This application demonstrates a Solution for character building 
+ *  
+ * Version: 0.0.1 - added all properties to splash form
+ */
+namespace COMP123_S2016_FinalExam
+{
+    public partial class SplashForm : Form
+    {
+    /**  
+    * <summary>  
+    * This is the partial SplashForm class for starting splash.  
+    * </summary>  
+    *   
+    * @class SplashForm  
+    */
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        //PRIVATE METHODES++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        /**
+       * <summary>
+       * This is the privte method for _Tick event of SplashTimer
+       * </summary>
+       * 
+       * @method SplashTimer_Tick
+       * @returns {void} 
+       * @param {Object} sender
+       * @param {EventArgs} e
+       * 
+       */
+        private void SplashTimer_Tick(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
